8a58caa3d81003e53f4635b01bdb27f4a3c919bc
Deep <diphiwase0987@gmail.com>
2025-09-11 14:27:57 +0530

    Add cors

--- src/server.ts
+++ src/server.ts
@@ -7,10 +7,72 @@
  * Node Modules
  */
 import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import compression from "compression";
+import helmet from "helmet";
 
 /**
- * Express app initial
+ * Custom modules
  */
+import config from "@/config";
+
+/**
+ * Types
+ */
+import type { CorsOptions } from "cors";
+
 const app = express();
 
-app.listen(4004);
+const corsOptions: CorsOptions = {
+  origin(requestOrigin, callback) {
+    if (
+      config.NODE_ENV === "development" ||
+      !requestOrigin ||
+      config.WHITELIST_ORIGINS.includes(requestOrigin)
+    ) {
+      callback(null, true);
+    } else {
+      callback(
+        new Error(`CORS error: ${requestOrigin} is not allowed by CORS`),
+        false,
+      );
+      console.log(`CORS error: ${requestOrigin} is not allowed by CORS`);
+    }
+  },
+};
+
+app.use(cors(corsOptions));
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
+// app.use(cookieParser());
+// app.use(
+//   compression({
+//     threshold: 1024,
+//   }),
+// );
+// app.use(helmet());
+
+// (async () => {
+//   try {
+//   } catch (err) {}
+// })();
+
+// const handleServerShutdown = async () => {
+//   try {
+//     process.exit(0);
+//   } catch (err) {}
+// };
+
+// process.on("SIGTERM", handleServerShutdown);
+// process.on("SIGINT", handleServerShutdown);
+
+app.get("/", (req, res) => {
+  res.json({
+    message: "hello world",
+  });
+});
+
+app.listen(config.PORT, () => {
+  console.log("Server is running");
+});
