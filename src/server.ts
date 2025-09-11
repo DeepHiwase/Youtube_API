6e4aad856acbbfe84e77d0ff5cdc8b9b60722a93
Deep <diphiwase0987@gmail.com>
2025-09-11 11:51:06 +0530

    Config prettier, husky, lint-staged, Add License, .env with example file, server.ts. Add Nodemon script

---
+++ src/server.ts
@@ -0,0 +1,16 @@
+/**
+ * @copyright 2025 deephiwase
+ * @license Apache-2.0
+ */
+
+/**
+ * Node Modules
+ */
+import express from "express";
+
+/**
+ * Express app initial
+ */
+const app = express();
+
+app.listen(4004);
